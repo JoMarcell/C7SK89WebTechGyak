8b290140684b6537bbafa9b715c15a6360cf8b34
JoMarcell <josvaimarcell@gmail.com>
2021-04-28 12:02:10 +0200

    tizenegyedik ora

---
+++ C7SK89_0428/feldolgozo2.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="hu">
+ <head>
+  <meta charset="UTF-8">
+
+  <title>Document</title>
+ </head>
+ <body>
+
+ <font style="font-size:18pt">
+
+ <h2>Űrlap vezérlése új elemekkel</h2>
+
+
+	<?php
+
+ print ("Név: " . $_GET['nev'] . "<br>");
+
+ print ("PIN kód: " . $_GET['kod'] . "<br>");
+
+
+ ?>
+
+<a href="urlap3.html">vissza az űrlapra...</a>
+  
+ </body>
+</html>
